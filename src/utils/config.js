2d6413340f24f11f7a4a2e0a26200175a6f61889
Adhitvora <voraadhit2096@gmail.com>
2026-01-31 15:25:52 +0530

    Update config.js to use REACT_APP_MODE for environment detection

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const stripe_sky = 'pk_test_51Nk8Y4F0B89ncn3xWB6ZN3GsbVIVL7Jqfa3jxtIOpPkKHcleHZw4EMPJKd4cRwm34ZARBeYmAWwu3VxyYL1gb6OT00UKNSvfvb'
-const mode = process.env.NODE_ENV; 
+const mode = process.env.REACT_APP_MODE;
 
 let app_url, api_url;
 
@@ -10,8 +10,6 @@ if (mode === 'production') {
   app_url = 'http://localhost:3000';
   api_url = 'http://localhost:5000';
 }
-
-
 
 export {
     app_url,
