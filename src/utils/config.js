ac5b141e7904fca6dc5cec6e0cbb8ca806489b56
Adhitvora <voraadhit2096@gmail.com>
2026-01-31 15:15:46 +0530

    Refactor config.js to use NODE_ENV for environment detection and clean up code

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,20 +1,17 @@
 const stripe_sky = 'pk_test_51Nk8Y4F0B89ncn3xWB6ZN3GsbVIVL7Jqfa3jxtIOpPkKHcleHZw4EMPJKd4cRwm34ZARBeYmAWwu3VxyYL1gb6OT00UKNSvfvb'
+const mode = process.env.NODE_ENV; 
 
+let app_url, api_url;
 
-const production = 'production'
-const dev = 'development'
-
-const mode = dev
-
-let app_url, api_url
-
-if (mode === production) {
-    app_url = "https://e-commerce-store-client-frontend-hgjpax9ks-adhit-voras-projects.vercel.app"
-    api_url = "https://e-commerce-store-backend-2rgq.onrender.com"
+if (mode === 'production') {
+  app_url = 'https://e-commerce-store-client-frontend-hgjpax9ks-adhit-voras-projects.vercel.app';
+  api_url = 'https://e-commerce-store-backend-2rgq.onrender.com';
 } else {
-    app_url = 'http://localhost:3000'
-    api_url = 'http://localhost:5000'
+  app_url = 'http://localhost:3000';
+  api_url = 'http://localhost:5000';
 }
+
+
 
 export {
     app_url,
